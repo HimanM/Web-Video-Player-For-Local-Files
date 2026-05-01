3927681218f5bfd21729b619190bbbef6ee39ab3
HimanM <67066047+HimanM@users.noreply.github.com>
2026-05-02 05:13:15 +0530

    chore: add out directory to gitignore

--- src/app/page.tsx
+++
@@ -1,9 +0,0 @@
-import VideoPlayer from '@/components/VideoPlayer/VideoPlayer';
-
-export default function Home() {
-  return (
-    <main className="min-h-screen bg-black overflow-hidden font-sans antialiased">
-      <VideoPlayer />
-    </main>
-  );
-}
